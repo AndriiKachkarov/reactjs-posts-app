14f2aa3ac38972b246691b45a3eaf17c8f73464e
Andrii Kachkarov <Andriy.Kachkarov@gmail.com>
2020-12-21 12:42:23 +0200

    Create components and styles

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Andrii Kachkarov</h1>
+            <h2>5 posts, 2 liked</h2>
+        </div>
+    )
+};
+
+export default AppHeader;
